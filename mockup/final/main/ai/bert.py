0114f0f30ee0a7a8553cd1b651f81134c48a939b
pulpo125 <kang.pulpo@gmail.com>
2023-09-27 21:11:29 +0900

    하영 - mockup

---
+++ mockup/final/main/ai/bert.py
@@ -0,0 +1,27 @@
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+
+model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+
+# title -> embedding 변환
+def encoding(new_title):
+    new_embedding = model.encode(new_title)
+    return new_embedding
+
+# 코사인 유사도
+def cos_sim(A, B):
+  return dot(A, B)/(norm(A)*norm(B))
+
+# 임베딩 값 str -> array로 변환
+def emd_to_array(ply_title_embedding):
+   emd_array = ply_title_embedding.apply(lambda x: np.fromstring(x['ply_title_emd'].strip('[]'),dtype=float, sep=' '), axis=1)
+   return emd_array
+
+# 가장 유사한 플레이리스트 id list 추출하는 함수
+def get_sim_ply_id(ply_title_embedding, new_embedding, n=2):
+    ply_title_embedding['cos_sim'] = ply_title_embedding.apply(lambda x: cos_sim(x['ply_title_emd'], new_embedding), axis=1)
+    sim_ply_id = list(ply_title_embedding.sort_values(by='cos_sim', ascending=False)['ply_id'][:n])
+    return sim_ply_id

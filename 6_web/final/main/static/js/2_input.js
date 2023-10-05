257e05203f44727d339b3e603703824a6106de6a
pulpo125 <kang.pulpo@gmail.com>
2023-10-06 07:23:14 +0900

    hayoung

--- 6_web/final/main/static/js/2_input.js
+++ 6_web/final/main/static/js/2_input.js
@@ -46,6 +46,54 @@ function enterBtn() {
 }
 
 
+function enterBtn() {
+    // show div
+    const showdiv = document.getElementById("showBox")
+
+    // 버튼 클릭 시 div가 나타나기
+    if (showdiv.style.display === "none") {
+        showdiv.style.display = "block"; // 보이게 설정
+    } else {
+        showdiv.style.display = "none"; // 숨김 설정
+    }
+
+    let newTitle = $("input[name='new_title']").val();
+
+    // AJAX 요청
+    $.ajax({
+        url: "similar_ply",
+        method: "POST",
+        dataType: 'json',
+        data: {
+            csrfmiddlewaretoken: "{{ csrf_token }}",  // CSRF 토큰 전달
+            new_title: newTitle
+        },
+
+        success: function (response) {
+            var sim_ply_info = response["sim_ply_info"];
+            var ply_title = sim_ply_info[1]['ply_title'];
+
+            var newHtml = ''
+            for (const info of sim_ply_info) {
+                newHtml += '<div id="ply_' + info['ply_id'] + '">'
+                    + '<input class="checkbox" type="checkbox" name="select_ply" value="' + info['ply_id'] + '"/>'
+                    + '<details>'
+                    + '<summary><p class="title">' + info['ply_title'] + '</p></summary>'
+                    + '<div class="songbox">';
+                for (const meta of info['song_meta']) {
+                    newHtml += '<p>' + meta["song_name"] + ' - ' + meta["artist_name"] + '</p>';
+                }
+                newHtml += '</div>'
+                + '</details>'
+                + '</div>';
+            }
+
+            $("#plyContent").html(newHtml);
+        }
+    });
+}
+
+
 var slider1 = document.getElementById("mySlider1");
 var sliderValueDisplay1 = document.getElementById("sliderValueDisplay1");
 var slider2 = document.getElementById("mySlider2");
@@ -62,37 +110,15 @@ imageContainer.addEventListener('mouseleave', () => {
     hoverImage.style.opacity = '0'; // 호버가 끝나면 이미지가 숨겨짐
 });
 
-slider1.oninput = function() {
+slider1.addEventListener('input', function() {
     var sliderValue1 = this.value;
     sliderValueDisplay1.innerText = sliderValue1;
-    updateValue();
-    // 슬라이더1 값이 변경될 때 호출할 작업을 여기에 추가
-    // 슬라이더1 값이 변경될 때마다 콘솔에 로그 출력
     console.log("슬라이더 1 값이 변경됨:", sliderValue1);
-}
+});
 
-slider2.oninput = function() {
+slider2.addEventListener('input', function() {
     var sliderValue2 = this.value;
     sliderValueDisplay2.innerText = sliderValue2;
-    updateValue();
-    // 슬라이더2 값이 변경될 때 호출할 작업을 여기에 추가
-    // 슬라이더2 값이 변경될 때마다 콘솔에 로그 출력
     console.log("슬라이더 2 값이 변경됨:", sliderValue2);
-}
+});
 
-// 버튼 위치를 변경하는 함수
-function updateSliderPosition1(newPosition) {
-    slider1.value = newPosition;
-    sliderValueDisplay1.innerText = newPosition;
-}
-
-function updateSliderPosition2(newPosition) {
-    slider2.value = newPosition;
-    sliderValueDisplay2.innerText = newPosition;
-}
-
-// function updateValue() {
-//     // 슬라이드바 값이 변경될 때 호출할 작업을 여기에 추가
-//     // 슬라이드바 값이 변경될 때마다 콘솔에 로그 출력
-//     console.log("슬라이드바 값이 변경됨:", slider1.value, slider2.value);
-// }
--- 6_web/final/main/static/js/4_output.js
+++ 6_web/final/main/static/js/4_output.js
@@ -1,11 +1,11 @@
 function downloadImage() {
     // 이미지 파일 URL 설정 
-    var imageUrl = '/static/result/4_2_ex_img.png';  
+    var imageUrl = '/static/result/pubao.jpg';  
 
     // 새로운 <a> 요소를 동적으로 생성하여 이미지 다운로드 링크 생성
     var a = document.createElement('a');
     a.href = imageUrl;  // 이미지 파일 URL을 링크의 href 속성에 설정
-    a.download = '4_2_ex_img.png';  // 다운로드할 파일 이름 설정
+    a.download = 'pubao.jpg';  // 다운로드할 파일 이름 설정
     a.style.display = 'none';  // 화면에 표시하지 않도록 설정
 
     // <a> 요소를 body에 추가하여 클릭 이벤트를 발생
@@ -14,11 +14,18 @@ function downloadImage() {
 
     // <a> 요소를 삭제
     document.body.removeChild(a);
-}
 
+    // 데이터 URL 출력 (확인용)
+    console.log('Data URL:', imageUrl);
+}
 function captureAndDownload() {
-    // 현재 화면만 캡처하고 이미지로 저장
-    html2canvas(document.documentElement).then(function (canvas) { // document.documentElement은 현재 화면 전체를 나타냄
+    // 현재 화면 전체를 캡처하기 위해 document.body를 대상으로 설정
+    html2canvas(document.body, {
+        scrollX: 0, // 가로 스크롤을 없애기 위해 0으로 설정
+        scrollY: 0, // 세로 스크롤을 없애기 위해 0으로 설정
+        windowWidth: document.documentElement.offsetWidth, // 화면 너비 설정
+        windowHeight: document.documentElement.offsetHeight, // 화면 높이 설정
+    }).then(function (canvas) {
         // 이미지 데이터를 URL로 변환
         var image = canvas.toDataURL("image/png");
 
@@ -28,7 +35,11 @@ function captureAndDownload() {
         a.download = "screenshot.png"; // 다운로드할 파일 이름 설정
         a.style.display = "none";
         document.body.appendChild(a);
+
+        // 이미지 다운로드 링크를 클릭
         a.click();
+
+        // 다운로드를 위해 생성한 요소를 제거
         document.body.removeChild(a);
     });
 }
